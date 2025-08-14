fae4536bb7dd9ff0afe5235d898b5af2080dfc43
Tally <113806118+thenatally@users.noreply.github.com>
2025-08-14 14:57:30 -0400

    Create index.ts

---
+++ src/plugins/betterWindowBar/index.ts
@@ -0,0 +1,11 @@
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    authors: [Devs.tally],
+    name: "Better Window Bar",
+    description: "A plugin that improves the window bar by adding the freaking close and minimize buttons.",
+    patches: [
+
+    ]
+});
